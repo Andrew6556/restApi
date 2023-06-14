7d1edcd83ea5a3f00e279a2d5f8b9e97dee365a6
Andrey <andre.kozloff2004@gmail.com>
2023-06-15 01:53:07 +0300

    added a method for getting genres from a request for Cards

--- modules/Card.js
+++ modules/Card.js
@@ -2,7 +2,8 @@
 
 export class Card{
     constructor(data){
-        this.wrapper = document.createElement("div");
+        this.info_data = data;
+        this.wrapper   = document.createElement("div");
         this.wrapper.classList.add("card");
         this.wrapper.innerHTML = `
                 <div class="card__img">
@@ -14,7 +15,9 @@ export class Card{
                         <p class="card__text">Режиссер:<span class="card__decor">Классный</span></p>
                         <p class="card__text">Год выпуска:<span class="card__decor card__year">${data.year}</span></p>
                         <p class="card__text">Оценка зрителей:<span class="card__viewer-rating card__decor">${data.rating}</span></p>
+                        <p class="card__text">Жанры:<span class="card__viewer-rating card__decor">${this.genres()}</span></p>
                     </div>
                 </div>`;
     }
+    genres = () => this.info_data.genres.splice(0, 3).map(item => item.genre).join(", ");
 }
