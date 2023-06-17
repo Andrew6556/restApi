bf67577dfca36866b1132a2752f11650a1579709
Andrey <andre.kozloff2004@gmail.com>
2023-06-17 20:48:58 +0300

    fixed bug with sorting slider

--- modules/Card.js
+++ modules/Card.js
@@ -2,7 +2,6 @@
 
 export class Card{
     constructor(data){
-        this.info_data = data;
         this.wrapper   = document.createElement("div");
         this.wrapper.classList.add("card");
         this.wrapper.innerHTML = `
@@ -13,11 +12,10 @@ export class Card{
                 <div class="card__inner">
                     <div class="card__wrapper">
                         <p class="card__text">Режиссер:<span class="card__decor">Классный</span></p>
-                        <p class="card__text">Год выпуска:<span class="card__decor card__year">${data.year}</span></p>
-                        <p class="card__text">Оценка зрителей:<span class="card__viewer-rating card__decor">${data.rating}</span></p>
-                        <p class="card__text">Жанры:<span class="card__viewer-rating card__decor">${this.genres()}</span></p>
+                        <p class="card__text">Год выпуска:<span class="card__decor card__num">${data.year}</span></p>
+                        <p class="card__text">Оценка зрителей:<span class="card__viewer-rating card__decor card__num">${data.rating}</span></p>
+                        <p class="card__text">Жанры:<span class="card__decor">${data.genres[0].genre}</span></p>
                     </div>
                 </div>`;
     }
-    genres = () => this.info_data.genres.splice(0, 3).map(item => item.genre).join(", ");
 }
